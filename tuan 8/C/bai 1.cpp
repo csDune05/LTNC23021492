bfbe1b62ab83b3e5127183eb1f3abb700878c746
csDune05 <23021492@vnu.edu.vn>
2024-04-23 01:01:23 +0700

    commit

--- tuan 8/C/bai 1.cpp
+++ tuan 8/C/bai 1.cpp
@@ -1,38 +1,91 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
-struct String {
-    int length;
-    char* str;
-
-    String(const char* s) {
-        length = strlen(s);
-        str = new char[length + 1];
-        strcpy(str, s);
+size_t strlen(char a[]){
+    size_t length = 0;
+    while(*a != '\0'){
+        length++;
+        a++;
+    }
+    return length;
+}
+void reversea(char a[]) {
+    size_t l = strlen(a);
+    char* m = a;
+    char* n = a + l - 1;
+    while (m < n) {
+        char temp = *m;
+        *m = *n;
+        *n = temp;
+        m++;
+        n--;
+    }
+}
+void delete_char(char a[], char c) {
+    char* m = a;
+    char* n = a;
+    while (*m != '\0') {
+        if (*m != c) {
+            *n = *m;
+            n++;
+        }
+        m++;
+    }
+    *n = '\0';
+}
+void pad_right(char a[], int n){
+    size_t l =strlen(a);
+    if(l >= n) return;
+    char* m = a + l;
+    for(int i = l; i<n; i++){
+        *m = 'r';
+        m++;
+    }
+}
+void pad_left(char a[], int n){
+    size_t l =strlen(a);
+    if(l >= n) return;
+    char* m = a+l;
+    char* s = a+n;
+    while(m >= a){
+        *(s--) = *(m--);
+    }
+    for(int i =0; i<(n-l); i++){
+        *a = ' ';
+        a++;
+    }
+}
+void truncate(char a[], int m){
+    size_t l =strlen(a);
+    if(l <= m) return;
+    *(a+m) = '\0';
+}
+void trim_left(char a[]){
+    char *m = a;
+    char *n = a;
+    while (*m != '\0' && *m == ' ') {
+        m++;
+    }
+    while (*m != '\0') {
+        *n = *m;
+        n++;
+        m++;
     }
 
-    String() {
-        delete[] str;
+    *n = '\0';
+}
+void trim_right(char a[]){
+    size_t l = strlen(a);
+    char *m = a - l -1;
+    while (m >= a && *m == ' ') {
+        m--;
     }
-
-    void print() {
-        cout << str;
-    }
-
-    char* append(const char* s){
-        int newLength = length + strlen(s);
-        char* result = new char[newLength + 1];
-        strcpy(result, str);
-        strcat(result, s);
-        return result;
-    }
-};
-
+}
 int main() {
-    String greeting("Hi");
-    const char* appendedStr = greeting.append(" there");
-    cout << appendedStr << endl;
-    delete[] appendedStr;
-
+    char a[] = "lionelmessi      ";
+    char c = 's';
+    int n = 15;
+    int m = 10;
+    trim_right(a);
+    cout<<a;
     return 0;
 }
